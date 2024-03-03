ec6d5edb2cb7b0756c0b26b37f773df919e06066
ionicthor <ioniccompound6@gmail.com>
2024-03-03 11:17:45 +0530

    latest

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    // Get rid of the CORS error
     proxy: {
-      "/api": {
-        target: "https://backend-threadsclone.vercel.app/",
+      '/api': {
+        target: 'https://backend-threadsclone.vercel.app',
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
